f51ad83f907009a8ed724582b8e62d14bf1230e1
xiaobai <uiojkl3588@qq.com>
2025-12-25 21:00:27 +0800

    attribute aggregator

--- src/attributes/attribute_aggregator.rs
+++ src/attributes/attribute_aggregator.rs
@@ -1,25 +1,42 @@
-#[derive(Debug, Clone)]
+pub fn default_executor(aggregator: &Aggregator, base_value: f64) -> f64 {
+    if !aggregator.dirty {
+        return aggregator.cached;
+    }
+    if let Some(override_value) = aggregator.override_value {
+        return override_value;
+    }
+    let mut value = base_value;
+    for &add in &aggregator.additive {
+        value += add;
+    }
+    for &mul in &aggregator.multiplicative {
+        value *= mul;
+    }
+    value
+}
+
+#[derive(Clone)]
 pub struct Aggregator {
     additive: Vec<f64>,
     multiplicative: Vec<f64>,
     override_value: Option<f64>,
     cached: f64,
     dirty: bool,
+    executor: fn(&Aggregator, f64) -> f64,
 }
 
-impl Default for Aggregator {
-    fn default() -> Self {
+impl Aggregator {
+    pub fn new(executor: fn(&Aggregator, f64) -> f64) -> Self {
         Self {
             additive: Vec::new(),
             multiplicative: Vec::new(),
             override_value: None,
             cached: 0.0,
             dirty: true,
+            executor,
         }
     }
-}
 
-impl Aggregator {
     #[inline]
     pub fn make_dirty(&mut self) {
         self.dirty = true;
@@ -48,25 +65,10 @@ impl Aggregator {
     }
 
     pub fn evaluate(&mut self, base_value: f64) -> f64 {
-        if !self.dirty {
-            return self.cached;
+        if self.dirty {
+            self.cached = (self.executor)(self, base_value);
+            self.dirty = false;
         }
-
-        let mut value = base_value;
-
-        for add in &self.additive {
-            value += add;
-        }
-
-        for mul in &self.multiplicative {
-            value *= mul;
-        }
-
-        if let Some(override_value) = self.override_value {
-            value = override_value;
-        }
-        self.cached = value;
-        self.dirty = false;
-        value
+        self.cached
     }
 }
--- src/attributes/attribute_set.rs
+++ src/attributes/attribute_set.rs
@@ -7,25 +7,33 @@ pub const ATTRIBUTE_SET_SIZE: usize = GameplayAbilitySystemSettings::ATTRIBUTE_S
 #[derive(Component)]
 pub struct AttributeSet {
     attributes: Box<[Attribute]>,
-    _aggregator: Vec<Aggregator>,
+    aggregators: Vec<Aggregator>,
 }
 
 impl Default for AttributeSet {
     fn default() -> Self {
         Self {
             attributes: Box::new([Attribute::default(); ATTRIBUTE_SET_SIZE]),
-            _aggregator: vec![Aggregator::default(); ATTRIBUTE_SET_SIZE],
+            aggregators: vec![Aggregator::new(default_executor); ATTRIBUTE_SET_SIZE],
         }
     }
 }
 
 impl AttributeSet {
-    pub fn initialize_attribute(&mut self, id: AttributeId, base_value: f64) {
+    pub fn initialize_attribute(
+        &mut self,
+        id: AttributeId,
+        base_value: f64,
+        aggregator: Option<Aggregator>,
+    ) {
         let index = id.to_index();
         if index >= ATTRIBUTE_SET_SIZE {
             panic!("Exceeded ATTRIBUTE_SET_SIZE")
         }
         let attr = &mut self.attributes[index];
         attr.init(base_value);
+        if let Some(aggregator) = aggregator {
+            self.aggregators[index] = aggregator;
+        }
     }
 }
